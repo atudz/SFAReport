bf81475d3a10847c01445d81fa2c2016dce94aa4
Alex John Suarez <alexjohnsuarez@gmail.com>
2015-11-24 21:56:31 +0800

    [Front-end] added xeditable

--- resources/views/layout/assets/css.blade.php
+++ resources/views/layout/assets/css.blade.php
@@ -1,4 +1,6 @@
 <link rel="stylesheet" href="{{ URL::asset('js/components/packages/bootstrap/dist/css/bootstrap.min.css') }}" />
+<link rel="stylesheet" href="{{ URL::asset('js/components/packages/angular-xeditable/dist/css/xeditable.css') }}" />
+
 <link rel="stylesheet" href="{{ URL::asset('css/styles.css') }}" />
 
 <!--[if lt IE 9]>
