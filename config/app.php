0b35c1d67faf1283a58c3cfd27c21544bb823255
Abner Tudtud <atudtud@gmail.com>
2015-12-02 00:01:35 +0800

    Add laravel excel

--- config/app.php
+++ config/app.php
@@ -137,6 +137,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+    	Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -195,6 +196,7 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+    	'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
