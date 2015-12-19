fcbbd76f6274846beea8c67f2b958d77456c843b
Alex John Suarez <alexjohnsuarez@gmail.com>
2015-12-19 17:46:16 +0800

    starting

--- resources/views/User/myProfile.blade.php
+++ resources/views/User/myProfile.blade.php
@@ -122,8 +122,10 @@
 					</div>
 
 
-					<div class="toggle-save-button">
-						<a href=""><i class="fa fa-cog"></i></a>
+					<div class="rs-mini-toolbar">
+						<div class="rs-toolbar-savebtn">
+							<a class="button-primary revgreen" href="javascript:void(0)" id="button_save_slide-tb" original-title="" style="display: block;"><i class="fa fa-floppy-o" style="display: inline-block;vertical-align: middle;width: 25px;height: 20px;background-repeat: no-repeat;"></i>Save Profile</a>
+						</div>					
 					</div>
 
 				{!!Form::close()!!}
