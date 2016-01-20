1892c4ed19df882545771bb7b7eb154f3bc4c542
Abner Tudtud <atudtud@gmail.com>
2016-01-20 23:13:38 +0800

    Remove logs

--- resources/views/Reports/sync.blade.php
+++ resources/views/Reports/sync.blade.php
@@ -6,16 +6,15 @@
 		<div class="panel panel-default">
 			<div class="panel-body">
 				<div class="row">
-					<div class="col-sm-2 col-sm-offset-2">
-						<button class="btn btn-success" ng-click="sync()">Sync Now</button>&nbsp;&nbsp;&nbsp;
-						<span ng-show="showLoading"><i class="fa fa-spinner fa-pulse fa-2x"></i></span>										
+					<div style="margin-left: 38%;margin-right: 62%;width: 6em">
+						<button class="btn btn-success" ng-click="sync()">Sync Now</button>&nbsp;&nbsp;&nbsp;															
 					</div>					
 				</div>
 				<div class="row">
 					<div class="col-sm-6 col-sm-offset-2" style="padding-top:10px;margin-top:10px;">
-						<p class="alert-success" ng-show="showSuccess">Success!!!</p>
-						<p class="alert-warning" ng-show="showError">Failed!!!</p>
-						<pre>[[syncLogs]]</pre>																
+						<p class="text-center" ng-show="showLoading"><i class="fa fa-spinner fa-pulse fa-4x"></i></p>
+						<h2><p class="alert alert-success text-center" ng-show="showSuccess">Success!!</p></h2>
+						<h2><p class="alert alert-warning text-center" ng-show="showError">Failed!!</p></h2>																				
 					</div>
 				</div>
 			</div>			
