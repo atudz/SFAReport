c41736f1c4a7e39e5dcec13c013ba97f3615c167
jplagahit <jplagahit.dev@gmail.com>
2016-03-31 06:54:38 +0800

    Update logo and add favicon

--- resources/views/layout/assets/head.blade.php
+++ resources/views/layout/assets/head.blade.php
@@ -1,4 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<link rel="shortcut icon" type="image/x-icon" href="img/sfi_favicon.ico"/>
+
 <title>SFA SFI Reports - {{getenv('SETUP')}}</title>
