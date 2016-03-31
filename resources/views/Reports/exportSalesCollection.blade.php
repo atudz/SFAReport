e1b1cbe1973db0f3403b988fcd7bc3e842404922
jplagahit <jplagahit.dev@gmail.com>
2016-03-31 21:12:32 +0800

    Check if date is not null before formatting to date()

--- resources/views/Reports/exportSalesCollection.blade.php
+++ resources/views/Reports/exportSalesCollection.blade.php
@@ -71,7 +71,7 @@
 					@foreach($rows as $row)
 						<td align="left" style="wrap-text:true">
 							@if(is_object($record) && isset($record->$row))
-								@if(false !== strpos($row,'date'))
+								@if(false !== strpos($row,'date') && $record->$row)
 									{{ date('m/d/Y', strtotime($record->$row)) }}
 								@elseif(false !== strpos($record->$row,'.') && is_numeric($record->$row))	
 									{!!number_format($record->$row,2,'.',',')!!}	
@@ -79,7 +79,7 @@
 									{!!$record->$row!!}
 								@endif
 							@elseif(is_array($record) && isset($record[$row]))
-								@if(false !== strpos($row,'date'))
+								@if(false !== strpos($row,'date') && $record[$row])
 									{{ date('m/d/Y', strtotime($record[$row])) }}
 								@elseif(false !== strpos($record->$row,'.') && is_numeric($record->$row))	
 									{!!number_format($record->$row,2,'.',',')!!}	
