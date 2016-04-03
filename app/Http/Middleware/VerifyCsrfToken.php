358a832b7f50362f027ed642f19406af881772c5
Abner Tudtud <atudtud@gmail.com>
2016-04-03 16:44:04 +0800

    Fix token mismatch

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+use Closure;
 
 class VerifyCsrfToken extends BaseVerifier
 {
@@ -28,4 +29,23 @@ class VerifyCsrfToken extends BaseVerifier
     	'/controller/reports/save',
     	'/controller/user/save',
     ];
+    
+    
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     *
+     * @throws \Illuminate\Session\TokenMismatchException
+     */
+    public function handle($request, Closure $next)
+    {
+    	if ($this->isReading($request) || $this->shouldPassThrough($request) || $this->tokensMatch($request)) {
+    		return $this->addCookieToResponse($request, $next($request));
+    	}
+    
+    	return redirect()->guest('/login');
+    }
 }
