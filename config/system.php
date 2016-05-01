e82e733387aee7a0e21d60d00ffdb5d8bc20c757
Abner Tudtud <atudtud@gmail.com>
2016-05-01 23:14:26 +0800

    Update go live date

--- config/system.php
+++ config/system.php
@@ -31,7 +31,7 @@ return [
 	'report_limit_pdf' => '100',
 		
 	// Go live date for van inventory
-	'go_live_date' => '2016-05-02',
+	'go_live_date' => '2016-05-01',
 		
 	// Maximum admin users
 	'max_admin_users' => 6	
