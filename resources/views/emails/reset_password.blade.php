145016cf201112e1c5daf8eb7717a2ec56297dfe
Abner Tudtud <atudtud@gmail.com>
2016-05-16 00:45:30 +0800

    Update email

--- resources/views/emails/reset_password.blade.php
+++ resources/views/emails/reset_password.blade.php
@@ -1,5 +1,5 @@
 <p>Cheers!</p>
 
-<p>Global Admin password has been changed. The new password is {{$password}}.</p>
+<p>Global Admin password for @if(env('SETUP') == 'Production') Production @else Staging @endif has been changed. The new password is {{$password}}.</p>
 
 <p>{{$from}}</p>
