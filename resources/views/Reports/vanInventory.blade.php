5423985402dd3414c2d61dd7d4617e0936610a6a
Mark Gerald Cabatingan <markgeraldcabatingan@gmail.com>
2016-07-25 23:03:26 +0800

    remove the filter of data in auditor html select tag

--- resources/views/Reports/vanInventory.blade.php
+++ resources/views/Reports/vanInventory.blade.php
@@ -9,7 +9,7 @@
 				{!!Html::fopen('Toggle Filter')!!}
 					<div class="col-md-6">
 						{!!Html::select('salesman_code','Salesman', $salesman,'')!!}
-						{!!Html::select('audited_by','Audited By:', $auditor, $auditor ? '' : 'No data found', $auditor ?:['disabled' => true])!!}
+						{!!Html::select('audited_by','Audited By:', $auditor, '')!!}
 						{!!Html::input('text','invoice_number','Invoice #')!!}
 						{!!Html::input('text','stock_transfer_number','Stock Transfer #')!!}
 					</div>					
