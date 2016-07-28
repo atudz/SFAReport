945136f11597d4e4bf74b8aebebdf8d409762d6d
Mark Gerald Cabatingan <markgeraldcabatingan@gmail.com>
2016-07-28 22:04:49 +0800

    set up error message for contact us

--- resources/views/User/contactUs.blade.php
+++ resources/views/User/contactUs.blade.php
@@ -6,32 +6,32 @@
         <div class="panel panel-default">
             <div class="panel-body">
                 <div class="col-sm-6 well">
-                    {!!Html::error('personalInfoError','error_list_personal')!!}
+                    {!!Html::error('error','error_list_contact')!!}
                     <div class="row form-input-field">
-                        {!!Html::input('text','name','Name <span class="required">*</span>')!!}
+                        {!!Html::input('text','name','Name <span class="required">*</span>','', ['ng-model' => 'contact.name'])!!}
                     </div>
                     <div class="row form-input-field">
-                        {!!Html::input('text','phone','Phone <span class="required">*</span>')!!}
+                        {!!Html::input('text','phone','Phone <span class="required">*</span>','', ['ng-model' => 'contact.phone'])!!}
                     </div>
                     <div class="row form-input-field">
-                        {!!Html::input('text','email','Email <span class="required">*</span>')!!}
+                        {!!Html::input('text','email','Email <span class="required">*</span>','', ['ng-model' => 'contact.email'])!!}
                     </div>
                     <div class="row form-input-field">
                         {{--{!!Html::select('branch','Branch <span class="required">*</span>', '','')!!}--}}
-                        {!!Html::input('text','branch','Branch <span class="required">*</span>')!!}
+                        {!!Html::input('text','branch','Branch <span class="required">*</span>','', ['ng-model' => 'contact.branch'])!!}
                     </div>
                     <h4 style="margin-bottom: 20px;">Best Time To Call</h4>
                     <div class="row form-input-field">
-                        {!!Html::input('text','callFrom','From <span class="required">*</span>')!!}
+                        {!!Html::input('text','callFrom','From <span class="required">*</span>','', ['ng-model' => 'contact.callFrom'])!!}
                     </div>
                     <div class="row form-input-field">
-                        {!!Html::input('text','callTo','To <span class="required">*</span>')!!}
+                        {!!Html::input('text','callTo','To <span class="required">*</span>','', ['ng-model' => 'contact.callTo'])!!}
                     </div>
                     <div class="row form-input-field">
-                        {!!Html::input('text','subject','Subject <span class="required">*</span>')!!}
+                        {!!Html::input('text','subject','Subject <span class="required">*</span>','', ['ng-model' => 'contact.subject'])!!}
                     </div>
                     <div class="row form-input-field">
-                        {!!Html::input('text','comment','Comment <span class="required">*</span>')!!}
+                        {!!Html::input('text','comment','Comment <span class="required">*</span>','', ['ng-model' => 'contact.comment'])!!}
                     </div>
                 </div>
 
