79c361638f4e7378fc65cfd2153c4e85d9e1e8dc
Mark Gerald Cabatingan <markgerald.nst@gmail.com>
2016-09-05 16:20:54 +0800

    change the error message in date range for summary of incident report

--- resources/views/User/summaryOfIncidentReport.blade.php
+++ resources/views/User/summaryOfIncidentReport.blade.php
@@ -7,7 +7,6 @@
                 <!-- Filter -->
                 {!!Html::fopen('Toggle Filter')!!}
                 <div class="col-md-6">
-                    {!!Html::error('error','error_filter_contact')!!}
                     {!!Html::select('name','Full Name:', $name,'All')!!}
                     {!!Html::select('branch','Branch:', $branch, 'All')!!}
                     {!!Html::input('text','incident_no','Incident #:','')!!}
