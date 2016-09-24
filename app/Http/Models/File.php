027f778180736b9133cefbf40609f03b6a293469
Mark Gerald Cabatingan <markgeraldcabatingan@gmail.com>
2016-09-24 14:26:50 +0800

    add new fillable filename

--- app/Http/Models/File.php
+++ app/Http/Models/File.php
@@ -7,7 +7,7 @@ use App\Core\ModelCore;
 class File extends ModelCore
 {
     protected $table = 'files';
-    protected $fillable = ['path'];
+    protected $fillable = ['path', 'filename'];
     protected $dates = ['created_at', 'updated_at'];
 
     /**
