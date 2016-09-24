44948dffe1622a68e5a246c7e2c3dfba83022744
wang <wang>
2016-09-24 16:12:19 -0700

    Fixed

--- resources/views/Macros/Html/html_table_close_macro.php
+++ resources/views/Macros/Html/html_table_close_macro.php
@@ -15,7 +15,7 @@ Html::macro('tclose', function($paginate=true) {
         		<div class="modal-body">
 	        		 <form class="form-horizontal">
 	        		 <p class="text-center bold required error-edit" id="editError">Please input remarks to proceed.</p>
-	        		 <p class="bold"><EDIT [[params.name]]</p>
+	        		 <p class="bold">EDIT [[params.name]]</p>
 						  <div class="form-group">
 							    <label class="control-label col-sm-3"></label>
 							    <div class="col-sm-9">
@@ -42,7 +42,7 @@ Html::macro('tclose', function($paginate=true) {
         		<div class="modal-body">
         			<form class="form-horizontal">
         			<p class="text-center bold required error-edit" id="editError">Please input remarks to proceed.</p>
-	        		 	<p  class="bold"><EDIT [[params.name]]</p>
+	        		 	<p  class="bold">EDIT [[params.name]]</p>
 						<div class="form-group">
 						    <label class="control-label col-sm-3"></label>
 						    <div class="col-sm-9">
@@ -71,7 +71,7 @@ Html::macro('tclose', function($paginate=true) {
         		<div class="modal-body">
         			<form class="form-horizontal">
         			<p class="text-center bold required error-edit" id="editError">Please input remarks to proceed.</p>
-	        		 	<p  class="bold"><EDIT [[params.name]]</p>
+	        		 	<p  class="bold">EDIT [[params.name]]</p>
 						<div class="form-group">
 						    <label class="control-label col-sm-3"></label>
 						    <div class="col-sm-9" data-ng-controller="EditableColumnsCalendar">
