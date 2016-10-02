d060f46798c28453167a22b756b689ab11e7fd8e
Abner Tudtud <abner@plus65.com.sg>
2016-10-02 21:09:10 +0800

    Revert "add a filter if report is bir add format date both back end and front end"
    This reverts commit 540491cd13b4dfcf06ce2c80780ec93c387c04b0.

--- resources/views/Reports/exportXls.blade.php
+++ resources/views/Reports/exportXls.blade.php
@@ -38,11 +38,7 @@
 						<td align="left" style="wrap-text:true">
 							@if(is_object($record) && isset($record->$row))
 								@if(false !== strpos($row,'date') && $record->$row)
-									@if($report == 'bir')
-										{{ $record->$row }}
-									@else
-										{{ date('m/d/Y', strtotime($record->$row)) }}
-									@endif
+									{{ date('m/d/Y', strtotime($record->$row)) }}
 								@elseif(false !== strpos($record->$row,'.') && is_numeric($record->$row))	
 									{!!str_replace(array('%',')','(', ','),'', $record->$row)!!}	
 								@elseif(ctype_alnum($record->$row))
