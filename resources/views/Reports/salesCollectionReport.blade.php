00350015c72eb051f39713a13b4758f1820d66e8
Abner Tudtud <abner@plus65.com.sg>
2016-11-06 15:45:31 +0800

    Merge enhancement 449 part 2

--- resources/views/Reports/salesCollectionReport.blade.php
+++ resources/views/Reports/salesCollectionReport.blade.php
@@ -23,7 +23,7 @@
 			{!!Html::fclose()!!}
 			<!-- End Filter -->
 			
-			{!!Html::topen(['no_download'=>$isGuest2,'no_pdf'=>$isGuest1])!!}
+			{!!Html::topen(['no_download'=>$isGuest2,'no_pdf'=>($isGuest1 || $isManager)])!!}
 				{!!Html::theader($tableHeaders)!!}
 				<tbody>
 				<tr ng-repeat="record in records|filter:query" id=[[$index]] class=[[record.updated]]>
