b13c174fcf4f75f200f2a73fe2d3d99f445db929
Abner Tudtud <abner@plus65.com.sg>
2017-03-06 21:56:10 +0800

    Fix migration

--- database/migrations/2017_01_24_231436_fix_table_logs.php
+++ database/migrations/2017_01_24_231436_fix_table_logs.php
@@ -6,6 +6,20 @@ use App\Factories\ModelFactory;
 
 class FixTableLogs extends Migration
 {
+	/**
+	 * Generate revision number,
+	 * @param unknown $reportType
+	 * @param unknown $prefix
+	 * @return string
+	 */
+	function generate_revision($reportType, $prefix='')
+	{
+		$max = DB::table('report_revisions')->where('report',$reportType)->max('revision_number');
+		$lastCount = str_replace($prefix, '', $max);
+		$revision = $prefix . str_pad($lastCount+1, 10, '0', STR_PAD_LEFT);
+		return $revision;
+	}
+	
     /**
      * Run the migrations.
      *
@@ -41,7 +55,7 @@ class FixTableLogs extends Migration
         	{
         		$type = $this->getType($log->report_type);
         		\DB::table('report_revisions')->insert([
-        				'revision_number' => generate_revision($type),
+        				'revision_number' => $this->generate_revision($type),
         				'report' => $type,
         				'updated_at' => $log->created_at,
         				'created_at' => $log->updated_at,
