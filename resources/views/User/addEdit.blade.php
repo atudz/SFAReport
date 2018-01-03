ca71c39a4ba899f803c2d68a6eb5fceec15b16a0
Raymund Enso <raymundenso@gmail.com>
2018-01-03 16:10:10 +0800

    no default value for user,branch and assignment field

--- resources/views/User/addEdit.blade.php
+++ resources/views/User/addEdit.blade.php
@@ -5,21 +5,21 @@
 	<div class="col-lg-12">
 		<div class="panel panel-default">
 			<div class="panel-body">
-			
+
 				<div class="col-sm-12" style="margin-bottom:5px;padding-left:0px;padding-right:0px;">
 					<div class="pull-left col-sm-6 alert-success no-padding" ng-show="success">
 						<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<div class="error-list">User record saved successfully.</div>						
+						<div class="error-list">User record saved successfully.</div>
 					</div>
 					<div class="pull-right">
 			      		<a href="#user.list" class="btn btn-success btn-sm">Back to User List</a>
 			      	</div>
 			    </div>
-				
+
 				<div class="col-sm-12">
-				
+
 				</div>
-			
+
 				<div class="pull-left col-sm-6 well">
 					<h4 style="margin-bottom: 20px;">Personal Information</h4>
 					{!!Html::error('personalInfoError','error_list_personal')!!}
@@ -49,7 +49,7 @@
 					</div>
 					<div class="row form-input-field">
 						{!!Html::input('number','age','Age <span class="required">*</span>','',['min'=>18])!!}
-					</div>					
+					</div>
 					<div class="row form-input-field">
 						{!!Html::input('text','address','Address')!!}
 					</div>
@@ -58,7 +58,7 @@
 					</div>
 					<div class="row form-input-field">
 						{!!Html::input('text','mobile','Mobile No.')!!}
-					</div>					
+					</div>
 					<div class="row form-input-field">
 						{!!Html::input('text','salesman_code','Salesman Code <span id="span_salesman" class="required hidden">*</span>')!!}
 					</div>
@@ -78,23 +78,23 @@
 						</div>
 					</div>
 				</div>
-					
-					
+
+
 				<div class="pull-right col-sm-5 col-sm-offset-1 well">
 					<h4 style="margin-bottom: 20px;">Access & Location</h4>
 					{!!Html::error('locationInfoError','error_list_location')!!}
 					<div class="row form-input-field">
-						{!!Html::select('role','Role <span class="required">*</span>', $roles,'')!!}
+						{!!Html::select('role','Role <span class="required">*</span>', array_merge([''=>''],$roles),'')!!}
 					</div>
 					<div class="row form-input-field">
-						{!!Html::select('area','Branch <span class="required">*</span>', $areas, '')!!}
+						{!!Html::select('area','Branch <span class="required">*</span>', array_merge([''=>''],$areas), '')!!}
 					</div>
 					<div class="row form-input-field">
-						{!!Html::select('assignment_type','Assignment <span class="required">*</span>', $assignmentOptions, '')!!}
+						{!!Html::select('assignment_type','Assignment <span class="required">*</span>', array_merge([''=>''],$assignmentOptions), '')!!}
 					</div>
 					<div class="row form-input-field">
-						{!!Html::datepicker('assignment_date','Date From & To', true)!!}							
-					</div>												
+						{!!Html::datepicker('assignment_date','Date From & To', true)!!}
+					</div>
 				</div>
 				@if($navigationActions['can_save'])
 					<div class="rs-mini-toolbar">
@@ -103,11 +103,11 @@
 								<i class="fa fa-floppy-o" style="display: inline-block;vertical-align: middle;width: 25px;height: 20px;background-repeat: no-repeat;"></i>
 								Save
 							</a>
-						</div>					
+						</div>
 					</div>
 				@endif
-													
-			</div>			
+
+			</div>
 		</div>
 	</div>
 </div>
